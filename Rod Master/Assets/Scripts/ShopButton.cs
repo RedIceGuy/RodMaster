ec57571716968b15db2010738bd0bfa513dac432
anh0512 <thuvu0512@gmail.com>
2024-03-21 19:22:00 +0000

    fixed canvas

--- Rod Master/Assets/Scripts/ShopButton.cs
+++ Rod Master/Assets/Scripts/ShopButton.cs
@@ -1,17 +1,25 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopButton : MonoBehaviour
 {
     GameManager gm;
     [SerializeField]
     GameObject RodToPurchase;
+    public Button button;
+    public int price = 0;
 
     private void Awake() {
         gm = GameManager.Instance;
+        button = GetComponent<Button>();
+
+        if (gm.currency < price) {
+            button.interactable = false;
+        }
     }
 
-    public void PurchaseRod(int price) {
-        Debug.Log("Pressing the purchase button");
+    public void PurchaseRod() {
         gm.PurchaseRod(RodToPurchase, price);
+        button.interactable = false;
     }
 }
