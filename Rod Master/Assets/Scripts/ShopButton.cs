7525b546c6916ac8c50682a8dfc30e9987d1c784
RedIceGuy <eythor0156@gmail.com>
2024-03-23 17:28:00 +0000

    cant rebuy owned fishing rods

--- Rod Master/Assets/Scripts/ShopButton.cs
+++ Rod Master/Assets/Scripts/ShopButton.cs
@@ -4,21 +4,34 @@ using UnityEngine.UI;
 public class ShopButton : MonoBehaviour
 {
     GameManager gm;
+    ShopLogic sl;
     [SerializeField] GameObject RodToPurchase;
     Button button;
+    TMPro.TextMeshProUGUI priceText;
     public int price = 0;
 
     private void Awake() {
         gm = GameManager.Instance;
+        sl = ShopLogic.Instance;
         button = GetComponent<Button>();
+        priceText = GetComponentInChildren<TMPro.TextMeshProUGUI>();
 
-        if (gm.currency < price) {
+        // If the player already owns the fishing rod assigned to the button
+        if (sl.ownedFishingRods.Contains(RodToPurchase)) {
+            priceText.text = "Already owned";
+            button.interactable = false;
+        }
+        else if (gm.currency < price) {
             button.interactable = false;
         }
     }
 
     public void PurchaseRod() {
         gm.PurchaseRod(RodToPurchase, price);
+        if (sl.ownedFishingRods == null) {
+            Debug.LogWarning("Owned Rods is NULL!");
+        }
+        sl.ownedFishingRods.Add(RodToPurchase);
         button.interactable = false;
     }
 }
