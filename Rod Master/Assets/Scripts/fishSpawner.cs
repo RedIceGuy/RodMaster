7cc1fa9c9edac5e8102213a50c1a53f456e5aa3c
RedIceGuy <eythor0156@gmail.com>
2024-03-26 15:43:29 +0000

    Updated main level scene

--- Rod Master/Assets/Scripts/fishSpawner.cs
+++ Rod Master/Assets/Scripts/fishSpawner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FishSpawner1 : MonoBehaviour
+public class FishSpawner : MonoBehaviour
 {
     [SerializeField] GameObject[] fishesToSpawn;
     readonly int[] options = {8, -8};
