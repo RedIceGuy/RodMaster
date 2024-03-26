bb018900f2427a7720ff5254e4abe5fa109a0c11
bjarnibaerings <bjarnibh@outlook.com>
2024-03-26 13:29:54 +0000

    Added a min and max y position varibles for fish that dictates where they spawn

--- Rod Master/Assets/Scripts/fishSpawner.cs
+++ Rod Master/Assets/Scripts/fishSpawner.cs
@@ -32,7 +32,9 @@ public class fishSpawner : MonoBehaviour
     private IEnumerator spawnFish(float interval, GameObject fish)
     {
         yield return new WaitForSeconds(interval);
-        Instantiate(fish, new Vector3(options[Random.Range(0,2)],Random.Range(-4f,2.5f),0), Quaternion.identity);
+        Instantiate(fish, new Vector3(options[Random.Range(0,2)],
+        Random.Range(fish.GetComponent<Fish>().min_y,fish.GetComponent<Fish>().max_y),0), Quaternion.identity);
+        
         StartCoroutine(spawnFish(interval,fish));
     }
 }
