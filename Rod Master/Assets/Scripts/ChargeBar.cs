e5f50fe6bde3c22894ff61d4b55ffe5d9963e091
RedIceGuy <eythor0156@gmail.com>
2024-04-03 19:47:26 +0000

    Started work on a charge bar

--- Rod Master/Assets/Scripts/ChargeBar.cs
+++ Rod Master/Assets/Scripts/ChargeBar.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ChargeBar : MonoBehaviour
+public class ChargeBar : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] TMP_Text chargingText;
+    [SerializeField] float chargeIncrement;
+    [SerializeField] float currentCharge;
+    [SerializeField] float maxCharge = 100.0f;
+    [SerializeField] bool isCharging;
+   
+    // Player releases the charge button
+    public void OnPointerUp(PointerEventData eventData) {
+        isCharging = false;
+        chargingText.gameObject.SetActive(false);
+    }
+
+    // Player is holding the charge button
+    public void OnPointerDown(PointerEventData eventData) {
+        isCharging = true;
+        chargingText.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (isCharging && currentCharge < maxCharge) {
+            currentCharge += chargeIncrement * Time.deltaTime;
+        } 
+        else if (isCharging && currentCharge > maxCharge) {
+            currentCharge = maxCharge;
+        }
     }
 }
