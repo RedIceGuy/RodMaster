38fc2b43db4d20748d83e4d4fa21624f4f92f526
anh0512 <thuvu0512@gmail.com>
2024-04-09 15:05:59 +0000

    Reapply without changing Base
    This reverts commit fad77e3e43a6356e9933815f514f5a345af70e8f.

--- Rod Master/Assets/Scripts/junkSpawner.cs
+++ Rod Master/Assets/Scripts/junkSpawner.cs
@@ -5,22 +5,36 @@ using UnityEngine;
 public class junkSpawner : MonoBehaviour
 {
 
-    [SerializeField]
-    private GameObject junk1;
-
-    [SerializeField]
-    private float spawntimer = 4f;
+    [SerializeField] GameObject[] junkToSpawn;
+    public GameObject BoatLocation;
     // Start is called before the first frame update
-    int[] options = {10,-10};
+    float[] options = {10,-10};
     void Start()
     {
-        StartCoroutine(spawnJunk(spawntimer,junk1));
+        foreach (GameObject junkObject in junkToSpawn)
+        {
+            Junk junk = junkObject.GetComponent<Junk>();
+            StartCoroutine(SpawnJunk(junk.spawntimer,junkObject));
+        };
     }
 
-    private IEnumerator spawnJunk(float interval, GameObject junk)
+    void Update(){
+        options[0] = BoatLocation.transform.position.x +10;
+        options[1] = BoatLocation.transform.position.x -10;
+    }
+
+    private IEnumerator SpawnJunk(float interval, GameObject junk)
     {
         yield return new WaitForSeconds(interval);
-        GameObject newFish = Instantiate(junk, new Vector3(options[Random.Range(0,2)],Random.Range(-4f,2),0), Quaternion.identity);
-        StartCoroutine(spawnJunk(interval,junk));
+        Instantiate(
+            junk, 
+            new Vector3(
+                options[Random.Range(0, 2)], 
+                Random.Range(-4f, 2),
+                0
+            ), 
+            Quaternion.identity
+        );
+        StartCoroutine(SpawnJunk(interval,junk));
     }
 }
