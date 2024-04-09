9af0324e98c20f992bf55480bb749611aac1b520
anh0512 <thuvu0512@gmail.com>
2024-04-09 22:36:53 +0000

    Updated fish glossary

--- Rod Master/Assets/Scripts/GlossaryLogic.cs
+++ Rod Master/Assets/Scripts/GlossaryLogic.cs
@@ -7,14 +7,22 @@ public class GlossaryLogic : MonoBehaviour
 {
     [SerializeField] GameObject[] fishesToDisplay;
     private Image fish_image;
+    private TMPro.TextMeshProUGUI fish_name;
+    private TMPro.TextMeshProUGUI fish_value;
     private TMPro.TextMeshProUGUI fish_caught;
     void Update()
     {
         foreach (GameObject fishObject in fishesToDisplay) {
-            GameObject fish = fishObject.transform.GetChild(0).gameObject;
-            int quantity = PlayerPrefs.GetInt(fish.name, 0);
-            fish_caught = fishObject.transform.GetChild(2).gameObject.GetComponent<TMPro.TextMeshProUGUI>();
+            Fish fish = fishObject.transform.GetChild(0).gameObject.GetComponent<Fish>();
+            fish_name = fishObject.transform.GetChild(2).gameObject.GetComponent<TMPro.TextMeshProUGUI>();
+            fish_value = fishObject.transform.GetChild(3).gameObject.GetComponent<TMPro.TextMeshProUGUI>();
+            fish_caught = fishObject.transform.GetChild(4).gameObject.GetComponent<TMPro.TextMeshProUGUI>();
+
+            fish_name.text = fish.name;
+            fish_value.text = "$" + fish.value.ToString();
             fish_caught.text = "Caught: " + PlayerPrefs.GetInt(fish.name, 0).ToString();
+
+            int quantity = PlayerPrefs.GetInt(fish.name, 0);
             if (quantity == 0) {
                 fish_image = fish.GetComponent<Image>();
                 if (fish_image != null)
@@ -22,8 +30,6 @@ public class GlossaryLogic : MonoBehaviour
                     fish_image.color = Color.black;
                 }
             }
-            Debug.Log(fish.name);
-            Debug.Log(quantity);
         }
     }
 }
